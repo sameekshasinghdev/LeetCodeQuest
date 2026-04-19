5baa44de1460b344280bbe7b94734df4a8bc2693
Sameeksha Singh <sameeksha1209@gmail.com>
2026-04-19 14:38:49 +0530

    18042026

--- src/dailyPractice/EighteenFourTwentySix.java
+++ src/dailyPractice/EighteenFourTwentySix.java
@@ -1,10 +1,27 @@
 package dailyPractice;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class EighteenFourTwentySix {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	String s= "aabccdeff";
+	System.out.println("Find the first non-repeating character in a string: "+frstNonRepChar(s));
 	}
-
+	
+	public static char frstNonRepChar(String s){
+		LinkedHashMap<Character, Integer> freq = new LinkedHashMap<Character,Integer>();
+		
+		for(char ch: s.toCharArray()){
+			freq.put(ch,freq.getOrDefault(ch, 0)+1);
+		}
+		for (Entry<Character, Integer> entry : freq.entrySet()){
+			if(entry.getValue()==1) {
+				return entry.getKey();
+			}
+		}
+		return '_';
+	}
 }
