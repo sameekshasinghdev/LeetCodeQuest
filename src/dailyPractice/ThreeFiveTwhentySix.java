7d9fce8c609f35fc49683ef110b6bf4f5ca490da
Sameeksha Singh <sameeksha1209@gmail.com>
2026-05-03 01:01:20 +0530

    03/05/2026

---
+++ src/dailyPractice/ThreeFiveTwhentySix.java
@@ -0,0 +1,45 @@
+package dailyPractice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ThreeFiveTwhentySix {
+
+	public static void main(String[] args) {
+		// TODO STREAM API
+		//TODO BASIC LEVEL
+		//1. Filter Even Numbers
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6);
+		System.out.println("1. Filter Even Numbers: "+filterEvenNum(list));
+		List<String> sList = Arrays.asList("java", "spring", "boot");
+		System.out.println("2. Convert List<String> → Uppercase : "+convertToUpperCase(sList));
+		List<Integer>l1= Arrays.asList(5,8,12,20);
+		System.out.println("3. Find First Element > 10 : "+findFirstGreaterThanTen(l1));
+		List<String> l2 = Arrays.asList("Apple", "Banana", "Avocado", "Mango");
+		System.out.println("4. Count Strings starting with 'A' : "+countStringsStartingWithA(l2));
+	
+	
+	}
+
+	private static long countStringsStartingWithA(List<String> l2) {
+		// TODO Auto-generated method stub
+		return l2.stream().filter(s->s.startsWith("A")).count();
+	}
+
+	private static int findFirstGreaterThanTen(List<Integer> l1) {
+		// TODO Auto-generated method stub
+		return l1.stream().filter(i->i>10).findFirst().orElseThrow();
+	}
+
+	private static List<String> convertToUpperCase(List<String> sList) {
+		// TODO Auto-generated method stub
+		return sList.stream().map(s->s.toUpperCase()).toList();
+	}
+
+	private static List<Integer> filterEvenNum(List<Integer> list) {
+		
+		return list.stream().filter(i->i%2==0).toList();
+	}
+
+}
